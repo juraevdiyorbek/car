81fc288af14bee3816d2c47ae8441c0e6d9ac56e
juraevdiyorbek <juraevdiyor7@gmail.com>
2024-03-03 16:37:01 +0500

    almost finished

--- u9.py
+++ u9.py
@@ -1,18 +1,36 @@
 class Car:
-    def __init__(self,name,type,price,year) -> None:
+    def __init__(self,name,type,year,price = 4000) -> None:
         self.name = name
         self.type = type
         self.price = price
         self.year = year
-# Bu get_info() metodi mashina haqida hamma malumotlarni chiqar beradi
+
+    # Bu get_info() metodi mashina haqida hamma malumotlarni chiqar beradi
     def get_info(self):
         return f"""Name : {self.name}
 Type : {self.type}
-Price: {self.price}
+Price: ${self.price}
 Year : {self.year}"""
 # 1 metod yaratildi va chaqirilib ishlatildi
+    def setInfo(self,name,type,year,price = 4000):
+        self.name = name
+        self.type = type
+        self.year = year
+        self.price = price
+        return self.name,self.type,self.year,self.price
+    
+car = Car("Ferrari","turbo",2024,200000)
 
-car = Car("Ferrari","turbo",9999999,2024)
-print(car.get_info())
-
+# print(car.get_info())
+my_dict = dict()
+lst = list()
+for i in range(3):
+    name = input("name : ")
+    typee = input("type : ")
+    year = int(input("year : "))
+    price = input("price : ")
+    my_dict[i] = car.setInfo(name,typee,year,price)
+    lst.append(my_dict[i])
+lst.sort(key=lambda x : x[2])
+print(lst)
 
