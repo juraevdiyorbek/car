993145b280dd0082b54679e7e4970ba778316778
Diyorbek Juraev <85333075+juraevdiyorbek@users.noreply.github.com>
2024-03-03 15:42:42 +0500

    Update u9.py
    add comment

--- u9.py
+++ u9.py
@@ -4,13 +4,13 @@ class Car:
         self.type = type
         self.price = price
         self.year = year
-
+# Bu get_info() metodi mashina haqida hamma malumotlarni chiqar beradi
     def get_info(self):
         return f"""Name : {self.name}
 Type : {self.type}
 Price: {self.price}
 Year : {self.year}"""
-    
+# 1 metod yaratildi va chaqirilib ishlatildi
 
 car = Car("Ferrari","turbo",9999999,2024)
 print(car.get_info())
